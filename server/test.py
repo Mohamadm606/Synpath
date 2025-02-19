fbd952f62ee35b30bc492bdfcc8750d4f05e9b0c
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2025-02-19 15:49:38 -0500

    god

--- server/test.py
+++ server/test.py
@@ -1,6 +1,6 @@
-import ord_schema
-import ord_schema.message_helpers
+# import ord_schema
+# import ord_schema.message_helpers
 
-rxn = ord_schema.message_helpers.reaction_from_smiles("CCO >> CC=O")
+# rxn = ord_schema.message_helpers.reaction_from_smiles("CCO >> CC=O")
 
-print(rxn)
+# print(rxn)
--- src/components/MoleculeSketcher.jsx
+++ src/components/MoleculeSketcher.jsx
@@ -28,15 +28,15 @@ function MoleculeSketcher() {
     <div style={styles.container}>
       <div style={styles.box}>
         <h3>Start Mol</h3>
+        <h3>End Mol</h3>
+      </div>
+      <div style={styles.box}>
         <StructureEditor width="300px" onChange={(molfile, molecule, idCode) => handleChange(molfile, molecule, idCode, "start")}/>
+
+        <StructureEditor width="300px" onChange={(molfile, molecule, idCode) => handleChange(molfile, molecule, idCode, "end")}/>
       </div>
 
       <button style={styles.submitButton} onClick={handleSubmit}>submit:</button>
-
-      <div style={styles.box}>
-        <h3>End Mol</h3>
-        <StructureEditor width="300px" onChange={(molfile, molecule, idCode) => handleChange(molfile, molecule, idCode, "end")}/>
-      </div>
     </div>
   );
 }
@@ -44,15 +44,22 @@ function MoleculeSketcher() {
 const styles = {
   container: {
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     gap: "20px",
     padding: "20px",
   },
   box: {
+    display: "flex",
+    flexDirection: "row",
     border: "2px solid black",
     padding: "10px",
-    width: "300px",
+    width: "650px", // Increased width to fit both editors
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  editor: {
     textAlign: "center",
   },
   submitButton: {
